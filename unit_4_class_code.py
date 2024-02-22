3a9afe74a4617daf51220879e4926b6adf49532b
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-22 12:42:13 -0800

    sdlkj

--- unit_4_class_code.py
+++ unit_4_class_code.py
@@ -4,12 +4,26 @@ Date: 2/22/24
 Topic: Unit 4 - lists, tuples, list methods
 '''
 
-
 # Lists
+for i in range(1,11,2):
+    print(i,end=" ")
 
+odd_nums = list(range(1,11,2))
+print(odd_nums)
+# a list is a collection of items in Python 
+# that can change during the program
+even_nums = [2,4,6,8,10,"two","four","six","eight","ten"]
+print(even_nums)
 
-
+perfect_squares = []
 # List methods
+# append - adds an item to the end of a list
+perfect_squares.append(1)
+print(perfect_squares)
+# write a loop that fills the perfect squares
+# list with the squares from 1 to 144
+
+
 
 
 # Tuples
