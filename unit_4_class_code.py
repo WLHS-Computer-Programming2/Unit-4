00c91e4dfecc06f4abcfcbd0569f2f94188572e3
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-22 13:16:10 -0800

    .

--- unit_4_class_code.py
+++ unit_4_class_code.py
@@ -67,10 +67,17 @@ try:
 except ValueError:
     print("Not in list")
 
+# Tuple
+    
+my_tuple = (1,2,3,4)
+print(my_tuple)
+print(my_tuple[2])
+# my_tuple[3] = 5 this is not valid
 
+my_fav_foods[0] = "beef"
+my_tuple = (my_tuple[0],)
+print(my_tuple)
 
-
-# Tuples
 
 
 
