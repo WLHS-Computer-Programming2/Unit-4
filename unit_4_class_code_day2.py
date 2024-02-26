58028d5e0a8402e4d5e661584f437574021c4fec
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-26 12:29:07 -0800

    update

--- unit_4_class_code_day2.py
+++ unit_4_class_code_day2.py
@@ -8,3 +8,21 @@ my_list = ["taco","salad","pot roast","cake"]
 
 for item in my_list:
     print(item)
+
+# tuples in lists and lists in tuples
+
+#                   0      1     2       3
+#                  0 1    0 1   0 1    0   1        
+list_of_points = [(1,2),(-4,5),(7,4),(12,-12)]
+print(list_of_points[1]) # (-4,5)
+print(list_of_points[1][1]) # gives 5, the y coordinate of the point at index 1
+# list_of_points[1][1] = -5 this is invalid
+list_of_points[1] = (-4,-5)
+print(list_of_points)
+
+tuple_of_lists = ([1,2],[3,4],[5,6])
+print(tuple_of_lists[0][1]) # 2
+print(tuple_of_lists[0])
+# tuple_of_lists[0] = [-1,-2] invalid
+tuple_of_lists[0][0] = -1
+print(tuple_of_lists)
